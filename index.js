da3fd17f74f90bc0ef86802b1a4a0319dc369ba2
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-11 18:55:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function mondayWork(activity="go to the office") {
   return `This Monday, I will ${activity}.`;
 }
 
-function wrapAdjective() {
+function wrapAdjective(style=) {
   return function(adjective="special") {
     return `You are ${adjective}
   };
